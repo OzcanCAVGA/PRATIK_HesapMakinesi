2db376a69f3d4c553d5d79ebc1dadf90bc93b5b0
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:12:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int s1, s2, sonuc, tercih;
+        Scanner yazici = new Scanner(System.in);
+        System.out.println("Lutfen birinci sayiyi giriniz: ");
+        s1=yazici.nextInt();
+        System.out.println("Lutfen ikinci sayiyi giriniz: ");
+        s2=yazici.nextInt();
+        System.out.println("Islemler\n" +
+                "1 - Toplama\n" +
+                "2 - Cikarma\n" +
+                "3 - Carpma\n" +
+                "4 - Bolme\n");
+        System.out.println("Lutfen tercih ediniz: ");
+        tercih = yazici.nextInt();
+
+        switch (tercih) {
+            case 1:
+                System.out.println("Islem sonucu = " + (s1 + s2));
+                break;
+            case 2:
+                System.out.println("Islem Sonucu = "+(s1-s2));
+                break;
+            case 3:
+                System.out.println("Islem Sonucu = "+(s1*s2));
+                break;
+            case 4:
+                System.out.println("Islem Sonucu = "+(s1/s2));
+                break;
+        }
     }
 }
